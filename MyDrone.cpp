3fdd7a0af5ba58059739839c3ba24513000a817d
Leandro Lustosa <lustosa.leandro@gmail.com>
2016-04-17 19:02:27 +0200

    added elevon support and drone-on-fire mode

--- MyDrone.cpp
+++ MyDrone.cpp
@@ -1,13 +1,67 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "DroneFly.h"
+#include "DroneRoom.h"
 #include "MyDrone.h"
+#include "Engine/CurveTable.h"
 
 // Sets default values
 AMyDrone::AMyDrone()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Our root component will be a sphere that reacts to physics
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+	RootComponent = SphereComponent;
+	SphereComponent->InitSphereRadius(20.0f);
+	SphereComponent->BodyInstance.SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComponent->BodyInstance.SetResponseToAllChannels(ECR_Overlap);
+	OnActorBeginOverlap.AddDynamic(this, &AMyDrone::OnOverlapBegin);
+
+	// Create and position a mesh component to fuselage
+	UStaticMeshComponent* fuselageMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
+	fuselageMesh->AttachTo(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> FuselageVisualAsset(TEXT("/Game/unreal_fuselage.unreal_fuselage"));
+	if (FuselageVisualAsset.Succeeded())
+	{
+		fuselageMesh->SetStaticMesh(FuselageVisualAsset.Object);
+		fuselageMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f)); // no translation for now
+		fuselageMesh->SetWorldScale3D(FVector(1.0f)); // no scaling for now
+	}
+
+	// find elevon in assets library
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> ElevonVisualAsset(TEXT("/Game/elevon.elevon"));
+
+	// Create and position a mesh component to elevon 1
+	elevon1Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Elevon1Mesh"));
+	elevon1Mesh->AttachTo(RootComponent);
+	if (ElevonVisualAsset.Succeeded())
+	{
+		elevon1Mesh->SetStaticMesh(ElevonVisualAsset.Object);
+		elevon1Mesh->SetRelativeLocation(FVector(-12.0f, 12.0f, 0.0f)); // no translation for now
+		elevon1Mesh->SetWorldScale3D(FVector(1.0f)); // no scaling for now
+	}
+
+	// Create and position a mesh component to elevon 2
+	elevon2Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Elevon2Mesh"));
+	elevon2Mesh->AttachTo(RootComponent);
+	if (ElevonVisualAsset.Succeeded())
+	{
+		elevon2Mesh->SetStaticMesh(ElevonVisualAsset.Object);
+		elevon2Mesh->SetRelativeLocation(FVector(-12.0f, -12.0f, 0.0f)); // no translation for now
+		elevon2Mesh->SetWorldScale3D(FVector(1.0f)); // no scaling for now
+	}
+
+	// Create a particle system that we can activate or deactivate
+	DroneParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MovementParticles"));
+	DroneParticleSystem->AttachTo(fuselageMesh);
+	DroneParticleSystem->bAutoActivate = false;
+	DroneParticleSystem->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire"));
+	if (ParticleAsset.Succeeded())
+	{
+		DroneParticleSystem->SetTemplate(ParticleAsset.Object);
+	}
 
 	// inyit cvs table handlers
 	UCurveTable* expTable;
@@ -17,11 +71,11 @@ AMyDrone::AMyDrone()
 
 	// not sure what this is, but it seems necessary
 	static const FString ContextString(TEXT("GENERAL"));
-	
+
 	// here we link curves to experimental position CSV data
-	curveX = expTable->FindCurve( *FString::Printf(TEXT("X")), ContextString );
-	curveY = expTable->FindCurve( *FString::Printf(TEXT("Y")), ContextString );
-	curveZ = expTable->FindCurve( *FString::Printf(TEXT("Z")), ContextString );
+	curveX = expTable->FindCurve(*FString::Printf(TEXT("X")), ContextString);
+	curveY = expTable->FindCurve(*FString::Printf(TEXT("Y")), ContextString);
+	curveZ = expTable->FindCurve(*FString::Printf(TEXT("Z")), ContextString);
 
 	// here we link curves to experimental quaternion CSV data
 	curveQ0 = expTable->FindCurve(*FString::Printf(TEXT("Q0")), ContextString);
@@ -29,6 +83,10 @@ AMyDrone::AMyDrone()
 	curveQ2 = expTable->FindCurve(*FString::Printf(TEXT("Q2")), ContextString);
 	curveQ3 = expTable->FindCurve(*FString::Printf(TEXT("Q3")), ContextString);
 
+	// here we link curves to experimental elevon CSV data
+	curveD1 = expTable->FindCurve(*FString::Printf(TEXT("D1")), ContextString);
+	curveD2 = expTable->FindCurve(*FString::Printf(TEXT("D2")), ContextString);
+	
 }
 
 // Called when the game starts or when spawned
@@ -44,13 +102,13 @@ void AMyDrone::BeginPlay()
 	NewRotation.X = 0;
 	NewRotation.Y = 0;
 	NewRotation.Z = 0;
-	
+
 }
 
 // Called every frame
 void AMyDrone::Tick( float DeltaTime )
 {
-	Super::Tick( DeltaTime );
+	Super::Tick(DeltaTime);
 
 	FVector NewLocation;
 
@@ -81,8 +139,35 @@ void AMyDrone::Tick( float DeltaTime )
 	//NewRotation.Z = -FMath::Sin(theta/2)*FMath::Sin(theta / 2);
 	SetActorRotation(NewRotation);
 
+	// update elevon poses
+	float d1 = curveD1->Eval(RunningTime);
+	float d2 = curveD2->Eval(RunningTime);
+	Elevon1Rotation.Pitch = -d1; // sign is opposite to model standards
+	Elevon2Rotation.Pitch = -d2;
+	elevon1Mesh->SetRelativeRotation(Elevon1Rotation);
+	elevon2Mesh->SetRelativeRotation(Elevon2Rotation);
+
 	// here we update running time of actor
 	RunningTime += DeltaTime;
 
+}
+
+void AMyDrone::toggleFire()
+{
+	if (DroneParticleSystem && DroneParticleSystem->Template && !DroneParticleSystem->IsActive() ){
+		DroneParticleSystem->ToggleActive();
+	}
+}
+
+void AMyDrone::OnOverlapBegin(class AActor* OtherActor)
+{
+	// When we detect that we are overlapping with another actor we create a new component  
+	// above us, this is then removed when an actor leaves.  
+
+	// Other Actor is the actor that triggered the event. Check that is not ourself.  
+	if (OtherActor && (OtherActor != this))
+	{
+		toggleFire();
+	}
 }
 
--- MyDrone.h
+++ MyDrone.h
@@ -6,9 +6,12 @@
 #include "MyDrone.generated.h"
 
 UCLASS()
-class DRONEFLY_API AMyDrone : public AActor
+class DRONEROOM_API AMyDrone : public AActor
 {
 	GENERATED_BODY()
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+	USphereComponent* SphereComponent;
 
 	// here we stock each position component of the experiment
 	FRichCurve* curveX;
@@ -22,6 +25,10 @@ class DRONEFLY_API AMyDrone : public AActor
 	FRichCurve* curveQ2;
 	FRichCurve* curveQ3;
 
+	// here we stock elevon deflections in degrees!
+	FRichCurve* curveD1;
+	FRichCurve* curveD2;
+
 	// running time of the actor
 	float RunningTime;
 
@@ -30,6 +37,7 @@ class DRONEFLY_API AMyDrone : public AActor
 
 	// quaternion of actual attitude of the actor
 	FQuat NewRotation;
+
 
 public:	
 	// Sets default values for this actor's properties
@@ -40,5 +48,20 @@ public:
 	
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
+
+	// puts drone on fire
+	void toggleFire();
+
+	UFUNCTION()
+	void OnOverlapBegin(class AActor* OtherActor);
+
+	// particle system component
+	UParticleSystemComponent *DroneParticleSystem;
+	
+	// elevon mesh components and variables
+	UStaticMeshComponent* elevon1Mesh;
+	UStaticMeshComponent* elevon2Mesh;
+	FRotator Elevon1Rotation;
+	FRotator Elevon2Rotation;
 	
 };
